72566c41d38b4bdbc4364f66d32ba23a55042216
snishita <snishita@cs.takushoku-u.ac.jp>
2023-07-06 18:30:19 +0900

    第6回practice-ajax初期化

--- practice-event/event-intro.js
+++ practice-event/event-intro.js
@@ -2,15 +2,17 @@
 // 1. イベントハンドラの登録
 
 let b1 = document.querySelector('button#blue');
-b1.addEventListener('click', changeH1ColorRed);
+b1.addEventListener('click', changeH1ColorBlue);
 
 //// 以下，練習5-2 でコードを追加
+//let b2 = document.querySelector('button#random');
+//b2.addEventListener('click', changeBackgroundColorRandom);
 
 
 ////////////////////////////////////////////////////////
 // 2. イベントハンドラ（イベントが発生したら実行する関数）
 
-function changeH1ColorRed() {
+function changeH1ColorBlue() {
 	// 見出し h1 要素を検索
 	let h1 = document.querySelector('h1');
 
