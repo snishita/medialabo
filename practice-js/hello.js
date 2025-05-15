a0b5752e97b8d141916d282bc97675b4587c4edd
snishita <snishita@cs.takushoku-u.ac.jp>
2025-05-15 12:58:13 +0900

    practice-eventdom をを作成

--- practice-js/hello.js
+++ practice-js/hello.js
@@ -1,1 +1,15 @@
 console.log('こんにちは');
+let x;
+x = 3;
+x = x + 1;
+console.log(x);
+
+let cnt = 0;            // ローカル変数
+function foo(s) {
+    console.log(s);
+    cnt = cnt + 1;      // グローバル変数に1加える
+}
+
+foo('おはよう');
+foo('こんにちは');
+console.log(cnt);       // 2を出力
